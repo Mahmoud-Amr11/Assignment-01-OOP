50cc585886594da4e190ef160187e6fb6d0610a5
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-07-05 10:42:31 +0300

    Q3 solve

--- Assignment 01 OOP/Program.cs
+++ Assignment 01 OOP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel;
+using System.Reflection;
 
 namespace Assignment_01_OOP
 {
@@ -15,30 +16,51 @@ namespace Assignment_01_OOP
             #endregion
 
             #region 2-Create an Enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
-          
-            string name=Console.ReadLine();
-            season s;
-            bool isParsed = Enum.TryParse<season>(name, true, out s);
-            if (isParsed) { 
-               if (s == season.Spring)
-                {
-                    Console.WriteLine("Spring: March to May");
-                }
-                else if (s == season.Summer)
-                {
-                    Console.WriteLine("Summer: June to August");
-                }
-                else if (s == season.Autumn)
-                {
-                    Console.WriteLine("Autumn: September to November");
-                }
-                else if (s == season.Winter)
-                {
-                    Console.WriteLine("Winter: December to February");
-                }
 
-            }
+            //string name=Console.ReadLine();
+            //season s;
+            //bool isParsed = Enum.TryParse<season>(name, true, out s);
+            //if (isParsed) { 
+            //   if (s == season.Spring)
+            //    {
+            //        Console.WriteLine("Spring: March to May");
+            //    }
+            //    else if (s == season.Summer)
+            //    {
+            //        Console.WriteLine("Summer: June to August");
+            //    }
+            //    else if (s == season.Autumn)
+            //    {
+            //        Console.WriteLine("Autumn: September to November");
+            //    }
+            //    else if (s == season.Winter)
+            //    {
+            //        Console.WriteLine("Winter: December to February");
+            //    }
+
+            //}
             #endregion
+
+
+            #region 3 - Assign the following Permissions(Read, write, Delete, Execute) in a form of Enum Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission existed inside variable
+            Permissions permissions = Permissions.Write | Permissions.Delete; 
+            if((permissions & Permissions.Delete) == Permissions.Delete)
+            {
+                Console.WriteLine("Delete permission is allowed.");
+            }
+            if  ((permissions & Permissions.Write) == Permissions.Write)
+            {
+                Console.WriteLine("Write permission is allowed.");
+            }
+          
+            else
+            {
+                Console.WriteLine(" permission is not allowed.");
+            }
+
+            #endregion
+
+
         }
 
     }
@@ -61,5 +83,14 @@ namespace Assignment_01_OOP
         Summer,
         Autumn,
         Winter
+    }
+
+    [Flags]
+    enum Permissions :byte
+    {
+        Read=1,//00000001
+        Write =2,//00000010
+        Delete =4,//00000100
+        Execute = 8//00001000
     }
 }
