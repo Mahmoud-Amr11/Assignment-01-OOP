71fd0007e858a4c3ebb36b90ea9edc0833daa570
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-07-05 10:15:21 +0300

    Q1 solve

--- Assignment 01 OOP/Program.cs
+++ Assignment 01 OOP/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1-Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this Enum.
+            WeekDays[] daysOfWeek = (WeekDays[]) Enum.GetValues(typeof(WeekDays));
+                foreach (WeekDays day in daysOfWeek)
+                {
+                    Console.WriteLine(day);
+                }
+            #endregion
         }
+
+    }
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
     }
 }
