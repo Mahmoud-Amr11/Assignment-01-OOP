936800ce4904d8ddf3190707605d583da9e8f284
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-07-05 09:59:09 +0300

    Add project files.

---
+++ Assignment 01 OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_01_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
